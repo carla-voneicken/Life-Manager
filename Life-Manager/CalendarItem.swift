5c0d86d591f3af3d43b05a9516feb0b33246e553
carla-voneicken <carlavoneicken@googmail.com>
2025-03-31 17:11:30 +0200

    Created CalendarItem struct with variables and sample data

---
+++ Life-Manager/CalendarItem.swift
@@ -0,0 +1,49 @@
+//
+//  CalendarItemView.swift
+//  Life-Manager
+//
+//  Created by Carla von Eicken on 31.03.25.
+//
+
+import Foundation
+import SwiftUI
+
+struct CalendarItem {
+    var id: UUID = UUID()
+    var title: String
+    var description: String?
+    var time: String
+    var location: String?
+    var participants: [String]
+    
+    static var samples: [CalendarItem] {
+        [
+            CalendarItem(
+                title: "Zahnarzt",
+                description: "Prophylaxe",
+                time: "09:00 - 10:00",
+                location: "Praxis Zeit für schöne Zähne",
+                participants: ["Mama"]
+            ),
+            CalendarItem(
+                title: "Elterngespräch",
+                time: "14:15 - 14:45",
+                location: "Schule, Raum 1.12",
+                participants: [
+                    "Mama",
+                    "Papa",
+                    "Sophie"
+                ]
+            ),
+            CalendarItem(
+                title: "Fußball",
+                time: "15:30 - 17:00",
+                location: "Sportverein",
+                participants: [
+                    "Mama",
+                    "Ben"
+                ]
+            )
+        ]
+    }
+}
--- Life-Manager/CalendarItemView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CalendarItemView.swift
-//  Life-Manager
-//
-//  Created by Carla von Eicken on 31.03.25.
-//
-
-import Foundation
