3b072caba479d2a136d66109fdf7c184b04c8131
carla-voneicken <carlavoneicken@googmail.com>
2025-03-31 17:26:01 +0200

    Created View for single calendar item

--- Life-Manager/CalendarItem.swift
+++ Life-Manager/CalendarItem.swift
@@ -8,13 +8,48 @@
 import Foundation
 import SwiftUI
 
-struct CalendarItem {
+struct CalendarItem: View {
     var id: UUID = UUID()
     var title: String
     var description: String?
     var time: String
     var location: String?
     var participants: [String]
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.headline)
+                if description != nil {
+                    Text(description!)
+                        .font(.caption2)
+                }
+                Spacer()
+                if location != nil {
+                    Text(location!)
+                        .font(.caption)
+                }
+            }
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text(time)
+                    .font(.subheadline)
+                HStack {
+                    ForEach(participants, id: \.self) { item in
+                        var firstLetter = item.prefix(1).lowercased()
+                        Image(systemName: "\(firstLetter).circle.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.green)
+                    }
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .frame(height: 80)
+    }
     
     static var samples: [CalendarItem] {
         [
@@ -27,6 +62,7 @@ struct CalendarItem {
             ),
             CalendarItem(
                 title: "Elterngespräch",
+                description: "Gespräch mit Frau Müller",
                 time: "14:15 - 14:45",
                 location: "Schule, Raum 1.12",
                 participants: [
@@ -46,4 +82,14 @@ struct CalendarItem {
             )
         ]
     }
+}
+
+#Preview {
+    CalendarItem(
+        title: "Zahnarzt",
+        description: "Prophylaxe",
+        time: "09:00 - 10:00",
+        location: "Praxis Zeit für schöne Zähne",
+        participants: ["Mama", "Sophie"]
+    )
 }
