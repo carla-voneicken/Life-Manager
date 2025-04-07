0b85fb6f0c59a16079f68051eb418b022c9d6a12
carla-voneicken <carlavoneicken@googmail.com>
2025-04-07 15:34:18 +0200

    Implemented TabView to switch between different main views

---
+++ Life-Manager/FamilyView.swift
@@ -0,0 +1,18 @@
+//
+//  FamilyView.swift
+//  Life-Manager
+//
+//  Created by Carla von Eicken on 07.04.25.
+//
+
+import SwiftUI
+
+struct FamilyView: View {
+    var body: some View {
+        Text("Here will be the Family View")
+    }
+}
+
+#Preview {
+    FamilyView()
+}
