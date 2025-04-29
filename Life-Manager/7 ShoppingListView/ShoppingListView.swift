00d90f2900900dd3d97c6dc1650d50b39bb0fc29
Christiane <Christiane>
2025-04-29 10:48:18 +0200

    Changed ShoppingListView

--- Life-Manager/7 ShoppingListView/ShoppingListView.swift
+++ Life-Manager/7 ShoppingListView/ShoppingListView.swift
@@ -1,108 +1,177 @@
+//
+//  TaskView.swift
+//  Life-Manager
+//
+//  Created by Christiane Roth on 29.04.25.
+//
+
 import SwiftUI
-class Supermarket: Identifiable, ObservableObject {
-    let id = UUID()
-    @Published var name: String
-    @Published var items: [ShoppingItem] = []
-    init(name: String) {
-        self.name = name
-    }
-}
-struct ShoppingItem: Identifiable {
+struct Item: Identifiable {
     let id = UUID()
     var name: String
 }
-struct ShoppingListView: View {
-    let startColor = Color("BackgroundColor")
-    let endColor = Color("SeColor")
-        @State private var supermarkets: [Supermarket] = [
-            Supermarket(name: "Edeka"),
-            Supermarket(name: "Aldi"),
-            Supermarket(name: "Lidl"),
-            Supermarket(name: "Rewe")
-        ]
-        @State private var showingAddSupermarket = false
-    var body: some View {
-        ZStack {
-            NavigationStack {
-                VStack {
-                    List {
-                        ForEach(supermarkets, id: \.id) { supermarket in
-                            NavigationLink {
-                                SupermarketDetailView(supermarket: supermarket)
-                            } label: {
-                                Text(supermarket.name)
-                            }
-                        }
-                        .onDelete(perform: deleteSupermarket)
-                        .scrollContentBackground(.hidden)
-                    }
-                    .listStyle(.plain)
-                    Button("Supermarkt hinzufügen") {
-                        showingAddSupermarket = true
-                    }
-                    .padding()
-                }
-                .navigationTitle("Einkaufsliste")
-                .sheet(isPresented: $showingAddSupermarket) {
-                    AddSupermarketView(supermarkets: $supermarkets, isPresented: $showingAddSupermarket)
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity) // Füge diesen Frame hinzu
-                     .background {
-                         LinearGradient(colors: [startColor, endColor], startPoint: .topLeading, endPoint: .bottomTrailing)
-                             .edgesIgnoringSafeArea(.all)
-                     }
-                }
-            }
-        }
-    func deleteSupermarket(at offsets: IndexSet) {
-        supermarkets.remove(atOffsets: offsets)
+class Supermarket: Identifiable, ObservableObject, Equatable { // Wichtig um zu erkennen wo welcher String hingeschrieben wird. Ob lhs und rhs gleich sind oder nicht
+    let id = UUID()
+    @Published var name: String
+    @Published var items: [Item] = []
+    @Published var color: Color  // Füge eine Farbe hinzu
+       init(name: String, color: Color = .blue) {  // Standardfarbe ist Blau
+           self.name = name
+           self.color = color
+    }
+    
+    // Equatable-Konformität - vergleicht die verschiedenen Id´s
+    static func == (lhs: Supermarket, rhs: Supermarket) -> Bool {
+        return lhs.id == rhs.id
     }
 }
-    struct AddSupermarketView: View {
-        @Binding var supermarkets: [Supermarket]
-        @State private var newSupermarketName = ""
-        @Binding var isPresented: Bool
-        var body: some View {
-            NavigationView {
-                Form {
-                    TextField("Supermarkt Name", text: $newSupermarketName)
-                    Button("Hinzufügen") {
-                        if !newSupermarketName.isEmpty {
-                            supermarkets.append(Supermarket(name: newSupermarketName))
-                            isPresented = false
-                        }
-                    }
-                    .disabled(newSupermarketName.isEmpty)
-                }
-                .navigationTitle("Neuer Supermarkt")
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button("Abbrechen") {
-                            isPresented = false
-                        }
-                    }
-                }
-            }
+struct ShoppingListView: View {
+    @State private var newSupermarketTitle: String = ""
+    @State private var supermarket: [Supermarket] = [
+        Supermarket(name: "Aldi", color: .red),
+        Supermarket(name: "Edeka", color: .yellow),
+        Supermarket(name: "Ikea", color: .purple),
+        Supermarket(name: "Bauhaus", color: .mint)
+    ]
+    @State private var selectedSupermarket: Supermarket?
+    @State private var showPopup: Bool = false
+    
+//  Hintergrundfarbe bei Auswahl
+    private func backgroundColor(for supermarket: Supermarket) -> Color {
+        if let selected = selectedSupermarket {
+            return selected == supermarket ? Color.green.opacity(0.5) : Color.blue.opacity(0.2)
+        } else {
+// Wenn selectedMission nil ist, gib eine Standardfarbe zurück.
+            return Color.blue.opacity(0.2)
         }
     }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack {
+                    Spacer()
+                  //  TextField("Neue Mission", text: $newMissionTitle)
+                  //      .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.green)
+                        .padding()
+                        .onTapGesture {
+                            showPopup = true
+                        }
+                }
+                .padding(.trailing, 10)
+
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(supermarket) { supermarket in
+                                                   SupermarketButtonView(supermarket: supermarket, selectedSupermarket: $selectedSupermarket)
+                        }
+                    }
+                }
+                if let selectedSupermarket = selectedSupermarket {
+                    SupermarketDetailView(supermarket: selectedSupermarket)
+                } else {
+                    EmptyView()
+                }
+                Spacer()
+            }
+       
+            .overlay(
+                           Group {
+                               if showPopup {
+                                   SupermarketPopup(showPopup: $showPopup, newSupermarketTitle: $newSupermarketTitle, addNewSupermarket: addNewSupermarket)
+                                       .transition(.scale)
+                               }
+                           }
+                     )
+                }
+        }
+     //   .alert(alertTitle, isPresented: $showAlert) { TextField("", text: $newMissionTitle)
+   //         Button("Hinzufügen") {
+     //           addNewMission()
+    //        }
+   //         Button("Abbrechen", role: .cancel) {}
+   //     }
+  //  }
+    func addNewSupermarket() {
+        if !newSupermarketTitle.isEmpty {
+            let newSupermarket = Supermarket(name: newSupermarketTitle, color: Color.random())
+            supermarket.append(newSupermarket)
+            newSupermarketTitle = ""
+        }
+        showPopup = false  //Popup schließen
+    }
+}
+
+struct SupermarketPopup: View {
+    @Binding var showPopup: Bool
+    @Binding var newSupermarketTitle: String
+    var addNewSupermarket: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("New Task")
+                .font(.headline)
+                .padding()
+            TextField("", text: $newSupermarketTitle)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            HStack {
+                Button("Hinzufügen") {
+                    addNewSupermarket()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding(.horizontal)
+                
+                Button("Abbrechen") {
+                    withAnimation {
+                        showPopup = false
+                    }
+                }
+                .padding()
+                .background(Color.pink)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding(.horizontal)
+            }
+        }
+        .frame(width: 300, height: 200)
+        .background(Color.pink.opacity(0.3))
+        .cornerRadius(10)
+        .padding()
+        .transition(.scale)
+        .zIndex(1)
+    
+       
+    }
+      
+}
+
 
 struct SupermarketDetailView: View {
-    let startColor = Color("BackgroundColor")
-    let endColor = Color("SeColor")
     @ObservedObject var supermarket: Supermarket
-    @State  var newItemName = ""
-    @State  var struckThroughItems2 = Set<UUID>()
+    @State private var newItemName = ""
+    @State private var struckThroughItem2 = Set<UUID>()
+    
     var body: some View {
         VStack {
             Text("Artikel für \(supermarket.name)")
                 .font(.title)
                 .padding()
             HStack {
-                TextField("Neues Item", text: $newItemName)
+                TextField("New Item", text: $newItemName)
                     .textFieldStyle(.roundedBorder)
                 Button {
                     if !newItemName.isEmpty {
-                        supermarket.items.append(ShoppingItem(name: newItemName))
+                        supermarket.items.append(Item(name: newItemName))
                         newItemName = ""
                     }
                 } label: {
@@ -112,15 +181,15 @@ struct SupermarketDetailView: View {
             }
             .padding()
             List {
-                ForEach(supermarket.items, id: \.id) { item in
+                ForEach(supermarket.items) { item in
                     HStack {
                         Text(item.name)
-                            .strikethrough(struckThroughItems2.contains(item.id), color: .red)
+                            .strikethrough(struckThroughItem2.contains(item.id), color: .red)
                             .onTapGesture {
-                                if struckThroughItems2.contains(item.id) {
-                                    struckThroughItems2.remove(item.id)
+                                if struckThroughItem2.contains(item.id) {
+                                    struckThroughItem2.remove(item.id)
                                 } else {
-                                    struckThroughItems2.insert(item.id)
+                                    struckThroughItem2.insert(item.id)
                                 }
                             }
                     }
@@ -129,12 +198,13 @@ struct SupermarketDetailView: View {
                     supermarket.items.remove(atOffsets: indexSet)
                 }
             }
-            .listStyle(.plain)
         }
-        .navigationTitle(supermarket.name)
     }
 }
+
+
+
 #Preview {
     ShoppingListView()
-       
 }
+
---
+++ Life-Manager/7 ShoppingListView/SupermarketButtonDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  ShoppingButtonDetailView.swift
+//  Life-Manager
+//
+//  Created by Christiane Roth on 29.04.25.
+//
+
+import SwiftUI
+struct SupermarketButtonView: View {
+    let supermarket: Supermarket
+    // Binding, damit Änderungen hier die Hauptansicht beeinflussen.
+    @Binding var selectedSupermarket: Supermarket?
+    
+    var body: some View {
+        Button {
+            selectedSupermarket = supermarket
+        } label: {
+            Text(supermarket.name)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .frame(width: 150, height: 150)
+                .background(supermarket.color)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .padding(10)
+        }
+    }
+}
+struct SupermarketButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Erstelle eine Beispielmission mit einer Farbe
+        let exampleSupermarket = Supermarket(name: "Test Supermarket")
+        exampleSupermarket.color = .orange
+        
+        @State var selectedSupermarket: Supermarket? = nil
+        
+        return SupermarketButtonView(supermarket: exampleSupermarket, selectedSupermarket: $selectedSupermarket)
+            .previewLayout(.sizeThatFits)
+    }
+}
+
